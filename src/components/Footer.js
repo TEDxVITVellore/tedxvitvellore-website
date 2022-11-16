ece306c96f8b3af6139b4b0da63fb60f029a6cd2
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-11-16 17:52:53 +0530

    added something fun

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,87 @@
-import React from 'react';
+import {React, useState, useCallback, useRef} from 'react';
 import '../stylesheets/Footer.css';
+import ReactCanvasConfetti from "react-canvas-confetti";
+import SnackBar from 'my-react-snackbar';
+
+const canvasStyles = {
+    position: "fixed",
+    pointerEvents: "none",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    left: 0
+  };
+
+  const sleep = ms => new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
 
 function Footer() {
+    const refAnimationInstance = useRef(null);
+
+  const getInstance = useCallback((instance) => {
+    refAnimationInstance.current = instance;
+  }, []);
+
+  const makeShot = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { y: 0.9 },
+        particleCount: Math.floor(200 * particleRatio)
+      });
+  }, []);
+
+  const fire = useCallback(() => {
+    makeShot(0.25, {
+      spread: 26,
+      startVelocity: 55
+    });
+
+    makeShot(0.2, {
+      spread: 60
+    });
+
+    makeShot(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 45
+    });
+  }, [makeShot]);
+    const [counter, setCounter] = useState(0);
+    var [open, setOpen] = useState(false);
+    async function handleClick(){
+        // Use updater function when new state is derived from old
+        setCounter(counter+1);
+        if(counter === 5){
+            fire();
+            setOpen(true);
+            setCounter(0);
+            await sleep(2000);
+            setOpen(false);
+        }
+      };
   return (
     <div className="footer-outer">
+    <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
         <div className="App outer">
         <div className="ending">
             <h2 className="italic">“Train yourself to let go of everything you fear to lose.”</h2>
         </div>
         <div className='footer-flex'>
-            <div>
+            <div onClick={handleClick}>
                 <img width="200px" src="/assets/tedxlogo.png" alt="Samsung logo"></img>
             </div>
             <div className="contents">
@@ -51,6 +123,14 @@ function Footer() {
         </div>
         </div>
         </div>
+        <SnackBar
+        className="snack"
+        open={open}
+        message="You found an easter 🚀 but not a gf yet 😂"
+        position='bottom-center'
+        type="success"
+        timeout='3000'
+      />
         
     </div>
   )
