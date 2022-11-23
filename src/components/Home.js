7b87a60e287f8de8c0f9b50aeef3b3b3f3fc674b
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-11-23 22:28:38 +0530

    readability improved

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,19 +32,19 @@ function Home() {
     <Fade>
     <div className="main-heading big-gap">
             <h2>What is TED?</h2>
-            <p className="grey gap">TED is an acronym for technology, entertainment, and design. It is a nonpartisan, nonprofit organization committed to spreading innovative ideas and exciting discoveries through 18-minute talks delivered in front of live audiences at TED conferences.TED is dedicated to researching and sharing knowledge that matters. Scientists, researchers, technologists, business leaders, artists, designers, and other world experts take the TED stage to present "Ideas Worth Spreading."</p>
+            <p className="grey gap half">TED is an acronym for technology, entertainment, and design. It is a nonpartisan, nonprofit organization committed to spreading innovative ideas and exciting discoveries through 18-minute talks delivered in front of live audiences at TED conferences.TED is dedicated to researching and sharing knowledge that matters. Scientists, researchers, technologists, business leaders, artists, designers, and other world experts take the TED stage to present "Ideas Worth Spreading."</p>
         </div>
         </Fade>
         <Fade>
         <div className="main-heading big-gap">
             <h2>What is TEDx?</h2>
-            <p className="grey gap">TEDx events are held at a local community level and are independently organized by volunteers. Much like TED, TEDx events center around short, prepared talks, performances, and demonstrations emphasizing ideas that provoke learning, inspiration, and wonder. The events lay emphasis on community and allow the speakers, who wouldn't usually get access to a stage, to speak and communicate their ideas. Like TED, TEDx encourages thought-provoking and impactful ideas and does not promote religious, commercial, or political bias/agenda.</p>
+            <p className="grey gap half">TEDx events are held at a local community level and are independently organized by volunteers. Much like TED, TEDx events center around short, prepared talks, performances, and demonstrations emphasizing ideas that provoke learning, inspiration, and wonder. The events lay emphasis on community and allow the speakers, who wouldn't usually get access to a stage, to speak and communicate their ideas. Like TED, TEDx encourages thought-provoking and impactful ideas and does not promote religious, commercial, or political bias/agenda.</p>
         </div>
         </Fade>
         <Fade>
         <div className="main-heading big-gap">
             <h2>What is <span className="highlight-red">TEDxVITVellore</span>?</h2>
-            <p className="grey gap">Our mission at TEDxVITVellore is to serve as a local forum for ideas within our community that inspire individuals to transform their lives, destinies, and, eventually, our planet. We are a group of experienced, motivated, and inquisitive individuals who are driven by the purpose of bringing significant change. We work hard to create an experience for the audience by hosting events with motivational speakers who are prime leaders in their fields and performers who will entice you with their talent. We strive each year on the VIT campus in Vellore to make our concept a reality.</p>
+            <p className="grey gap half">Our mission at TEDxVITVellore is to serve as a local forum for ideas within our community that inspire individuals to transform their lives, destinies, and, eventually, our planet. We are a group of experienced, motivated, and inquisitive individuals who are driven by the purpose of bringing significant change. We work hard to create an experience for the audience by hosting events with motivational speakers who are prime leaders in their fields and performers who will entice you with their talent. We strive each year on the VIT campus in Vellore to make our concept a reality.</p>
         </div>
         </Fade>
     </div>
